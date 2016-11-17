e9bf9c637fde2883a58bb8045e3c28161b3b7f2b
mateus <mateusdebem@hotmail.com>
2016-11-17 11:44:19 -0200

    Git ignore

--- routes/prices.js
+++ routes/prices.js
@@ -5,17 +5,60 @@ var router = express.Router();
 var models = require('../models');
 
 //-----------------------------------------------------------------------------
+// Array with States and Fuel names
+//-----------------------------------------------------------------------------
+
+  var stateNames = new Array(
+    "Acre", "Alagoas", "Amapa", "Amazonas", "Bahia", "Ceara", "Distrito Federal",
+    "Espirito Santo", "Goias", "Maranhao", "Mato Grosso", "Mato Grosso Do Sul",
+    "Minas Gerais", "Para", "Paraiba", "Parana", "Pernambuco", "Piaui", "Rio De Janeiro",
+    "Rio Grande Do Norte", "Rio Grande Do Sul", "Rondonia", "Roraima", "Santa Catarina",
+    "Sao Paulo", "Sergipe", "Tocantins"
+  );
+
+  var stateSelection = new Array(
+    "AC*ACRE", "AL*ALAGOAS", "AP*AMAPA", "AM*AMAZONAS", "BA*BAHIA", "CE*CEARA", "DF*DISTRITO@FEDERAL",
+    "ES*ESPIRITO@SANTO", "GO*GOIAS", "MA*MARANHAO", "MT*MATO@GROSSO", "MS*MATO@GROSSO@DO@SUL",
+    "MG*MINAS@GERAIS", "PA*PARA", "PB*PARAIBA", "PR*PARANA", "PE*PERNAMBUCO", "PI*PIAUI", "RJ*RIO@DE@JANEIRO",
+    "RN*RIO@GRANDE@DO@NORTE", "RS*RIO@GRANDE@DO@SUL", "RO*RONDONIA", "RR*RORAIMA", "SC*SANTA@CATARINA",
+    "SP*SAO@PAULO", "SE*SERGIPE", "TO*TOCANTINS"
+  );
+
+  var fuelNames = new Array("Gasolina", "Etanol", "GNV", "Diesel", "Diesel S10", "GLP");
+
+  var fuelSelection = new Array("487*Gasolina", "643*Etanol", "476*GNV", "532*Diesel", "812*Diesel@S10", "462*GLP");
+
+//-----------------------------------------------------------------------------
 // API Implementation
 //-----------------------------------------------------------------------------
 
 // GET prices listing
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-  //res.json(JSON.stringify(arrayList));
+  var states = new Array();
+
+  var statistics = new Array();
+
+  var statistic = {
+    fueltype: '',
+    consumer_price: {
+      cp_avgPrice: 0.0,
+      cp_stdDeviation: 0.0,
+      cp_minPrice: 0.0,
+      cp_maxPrice: 0.0,
+      cp_avgMargin: 0.0
+    },
+    distribution_price: {
+      dp_avgPrice: 0.0,
+      dp_stdDeviation: 0.0,
+      dp_minPrice: 0.0,
+      dp_maxPrice: 0.0
+    }
+  }
+
   var state = models.State.findAll({
     attributes: ['name']
   }).then(function(states){
-    res.json(states);
+    //res.json(states);
   });
 });
 
@@ -46,13 +89,22 @@ router.get('/web-scraper', function(req, res, next) {
   var currentState;
   // search for attributes
   models.State.findOne({ where: {name: 'Acre'} }).then(function(state) {
-    currentState = state;
+    if (state !== null) {
+      currentState = state;
+    } else {
+      res.json({status: 'DB:State ' + 'Acre' + ' not found'});
+    }
   })
 
   var currentFuel;
   // search for attributes
   models.FuelType.findOne({ where: {name: 'Gasolina'} }).then(function(fuel) {
-    currentFuel = fuel;
+    if (fuel !== null) {
+      currentFuel = fuel;
+    } else {
+      //TODO
+      res.json({status: 'DB:FuelType ' + 'Gasolina' + ' not found'});
+    }
   })
 
   var options = {
@@ -102,6 +154,7 @@ router.get('/web-scraper', function(req, res, next) {
         */
 
         var city_name = $(this).children().first().children().first().text();
+        var tdElem = $(this).children().first().next().next();
 
         models.City
         .findOrCreate({where: {name: city_name, state_pk: currentState.id}})
@@ -110,45 +163,38 @@ router.get('/web-scraper', function(req, res, next) {
           console.log(city.id);
           console.log(city.name);
 
-          var statistic = {
-            //fuel_type: "Gasolina",
-            consumer_price: {
-              cp_avgPrice: 0.0,
-              cp_stdDeviation: 0.0,
-              cp_minPrice: 0.0,
-              cp_maxPrice: 0.0,
-              cp_avgMargin: 0.0
-            },
-            distribution_price: {
-              dp_avgPrice: 0.0,
-              dp_stdDeviation: 0.0,
-              dp_minPrice: 0.0,
-              dp_maxPrice: 0.0
-            }
+          var consumer_price = {
+            cp_avgPrice: 0.0,
+            cp_stdDeviation: 0.0,
+            cp_minPrice: 0.0,
+            cp_maxPrice: 0.0,
+            cp_avgMargin: 0.0
+          };
+          var distribution_price = {
+            dp_avgPrice: 0.0,
+            dp_stdDeviation: 0.0,
+            dp_minPrice: 0.0,
+            dp_maxPrice: 0.0
           };
 
-          var tdElem = $(this).children().first().next().next();
-          console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-          console.log(tdElem.text());
-      
-          statistic.consumer_price.cp_avgPrice = tdElem.text();
+          consumer_price.cp_avgPrice = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
-          statistic.consumer_price.cp_stdDeviation = tdElem.text();
+          consumer_price.cp_stdDeviation = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
-          statistic.consumer_price.cp_minPrice = tdElem.text();
+          consumer_price.cp_minPrice = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
-          statistic.consumer_price.cp_maxPrice = tdElem.text();
+          consumer_price.cp_maxPrice = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
-          statistic.consumer_price.cp_avgMargin = tdElem.text();
+          consumer_price.cp_avgMargin = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
 
-          statistic.consumer_price.dp_avgPrice = tdElem.text();
+          distribution_price.dp_avgPrice = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
-          statistic.consumer_price.dp_avgPrice = tdElem.text();
+          distribution_price.dp_stdDeviation = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
-          statistic.consumer_price.dp_avgPrice = tdElem.text();
+          distribution_price.dp_minPrice = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
-          statistic.consumer_price.dp_avgPrice = tdElem.text();
+          distribution_price.dp_maxPrice = parseFloat(tdElem.text().replace(',', '.'));
           tdElem = tdElem.next();
 
           models.Statistic
@@ -156,16 +202,16 @@ router.get('/web-scraper', function(req, res, next) {
             city_pk: city.id, fuel_type_pk: currentFuel.id
           }, defaults: {
             // Consumer Price
-            cp_avgPrice: statistic.consumer_price.cp_avgPrice,
-            cp_stdDeviation: statistic.consumer_price.cp_stdDeviation,
-            cp_minPrice: statistic.consumer_price.cp_minPrice,
-            cp_maxPrice: statistic.consumer_price.cp_maxPrice,
-            cp_avgMargin: statistic.consumer_price.cp_avgMargin,
+            cp_avgPrice: consumer_price.cp_avgPrice,
+            cp_stdDeviation: consumer_price.cp_stdDeviation,
+            cp_minPrice: consumer_price.cp_minPrice,
+            cp_maxPrice: consumer_price.cp_maxPrice,
+            cp_avgMargin: consumer_price.cp_avgMargin,
             // Distribution Price
-            dp_avgPrice: statistic.distribution_price.dp_avgPrice,
-            dp_stdDeviation: statistic.distribution_price.dp_stdDeviation,
-            dp_minPrice: statistic.distribution_price.dp_minPrice,
-            dp_maxPrice: statistic.distribution_price.dp_maxPrice
+            dp_avgPrice: distribution_price.dp_avgPrice,
+            dp_stdDeviation: distribution_price.dp_stdDeviation,
+            dp_minPrice: distribution_price.dp_minPrice,
+            dp_maxPrice: distribution_price.dp_maxPrice
           }})
           .spread(function(statistic, created) {
             console.log(created);
